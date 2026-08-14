99d9e182ea8775a9a4c4ace294a6ff74663e6302
Megha706765 <meghathakur2003123@gmail.com>
2026-08-14 15:15:31 +0530

    first commit

---
+++ 02-folder/src/components/Card.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Card = () => {
+    return (
+        <div className='card'>
+            <h1>Card</h1>
+            <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Culpa, iusto?</p>
+        </div>
+    )
+}
+export default Card
---
+++ 02-folder/src/components/Navbar.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+function Navbar() {
+  return (
+    <div>
+      <div className='nav'>
+        <h1>This is the NavBar</h1>
+      </div>
+    </div>
+  )
+}
+
+export default Navbar
