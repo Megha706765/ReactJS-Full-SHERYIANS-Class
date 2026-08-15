f466bd971df014b9794386ba75236c42320c88fa
Megha706765 <meghathakur2003123@gmail.com>
2026-08-15 15:56:04 +0530

    CSS_File_Work

---
+++ 05-CSS/CSS-Work/src/components/Header.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import '../Style/Header.css'
+
+const Header = () => {
+  return (
+    <div className='header'>
+        <h3>Megha</h3>
+        <button>Login</button>
+    </div>
+  )
+}
+
+export default Header
